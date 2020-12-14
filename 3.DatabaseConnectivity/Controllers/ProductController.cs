4121bd3ab4bd341772fd35383d9c4df11ea0e6ae
unknown <admin@WJLP-1165.WinjitTechnologiesPvtLtd.local>
2020-12-14 13:02:55 +0530

    Database connectivity Completed

--- 3.DatabaseConnectivity/Controllers/ProductController.cs
+++ 3.DatabaseConnectivity/Controllers/ProductController.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using _3.DatabaseConnectivity.Config;
+using _3.DatabaseConnectivity.Models;
+using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -15,9 +19,49 @@ namespace _3.DatabaseConnectivity.Controllers
         }
 
         [HttpPost]
-        public ActionResult SaveProduct()
+        public ActionResult Create(Product product)
         {
-            return Content("Save Product");
+            using (SqlConnection con = new SqlConnection(StoreConnection.GetConnection()))
+            {
+                using (SqlCommand cmd = new SqlCommand("INSERT INTO Products(Name, Price, Supplier) VALUES ('"+product.Name+"','"+product.Price+"','"+product.Supplier+"')", con))
+                {
+                    if (con.State != System.Data.ConnectionState.Open)
+                        con.Open();
+                    cmd.ExecuteNonQuery();
+                }
+            }
+            return Content("Record is save in the database");
+        }
+
+        public ActionResult GetAll()
+        {
+            List<Product> products = new List<Product>();
+            using (SqlConnection con = new SqlConnection(StoreConnection.GetConnection()))
+            {
+                using (SqlCommand cmd = new SqlCommand("SELECT * FROM Products", con))
+                {
+                    if (con.State != System.Data.ConnectionState.Open)
+                        con.Open();
+
+                    SqlDataReader sdr = cmd.ExecuteReader();
+                    DataTable dtProducts = new DataTable();
+                    dtProducts.Load(sdr);
+
+                    foreach (DataRow row in dtProducts.Rows)
+                    {
+                        products.Add(
+                            new Product
+                            {
+                                Id = Convert.ToInt32(row["Id"]),
+                                Name = row["Name"].ToString(),
+                                Price = Convert.ToDecimal(row["Price"]),
+                                Supplier = row["Supplier"].ToString()
+                            }
+                         );
+                    }
+                }
+            }
+            return View(products);
         }
     }
 }
